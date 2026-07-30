ff88bf2467746f5697a8de183eea505a83ac270f
CHEONG WANG <b1214438548@gmail.com>
2026-07-30 16:27:03 +0800

    Align OTA headless default desktop version assert with 1.0.4

--- tools/firmware_ota_headless/src/main.rs
+++ tools/firmware_ota_headless/src/main.rs
@@ -335,7 +335,7 @@ mod tests {
         .expect("args");
 
         assert_eq!(args.desktop_version, repository_desktop_version().unwrap());
-        assert_eq!(args.desktop_version, "1.0.3");
+        assert_eq!(args.desktop_version, "1.0.4");
     }
 
     #[test]
